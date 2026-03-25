a41bb565cbddfb8579b5440e477f6001b794407c
Deandra Harvin <deandraharvin3@gmail.com>
2026-03-25 12:11:19 -0400

    adding backend and frontend for chatbot

---
+++ chatbot.py
@@ -0,0 +1,37 @@
+import os
+from openai import OpenAI
+
+client = OpenAI(
+        api_key="ADD"
+    )
+
+def chat_with_gpt(messages):
+    # if user_input.isdigit() and 1 <= int(user_input) <= len(prompts):
+    #     messages.append({"role": "user", "content": prompts[int(user_input) - 1]})
+    # else:
+    #     messages.append({"role": "user", "content": user_input})
+    response = client.responses.create(
+    model="gpt-5-nano",
+    tools=[{
+        "type": "web_search",
+        "user_location": {
+            "type": "approximate",
+            "country": "US",
+            "city": "Baltimore",
+            "region": "Maryland",
+        },
+        "filters":{
+            "allowed_domains": [
+                "www.google.com",
+                "www.opentable.com",
+                "www.eater.com",
+                "www.baltimoremagazine.com",
+                "www.yelp.com",
+            ]
+        }
+    }],
+    input=messages,
+    )
+    return response.output_text
+# if __name__ == "__main__":
+#     chat_with_gpt()
